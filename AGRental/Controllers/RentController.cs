af023f1e78aeea044d050fbc6e5fccbb1b3face2
destin9229 <Destin Attapallil@DESKTOP-2DOK1OI>
2019-02-22 14:18:14 -0500

    Added search bar rent section

--- AGRental/Controllers/RentController.cs
+++ AGRental/Controllers/RentController.cs
@@ -19,10 +19,51 @@ namespace AGRental.Controllers
         {
             context = dbContext;
         }
-
+        /*
         public IActionResult Index()
         {
             return View(context.Properties.ToList());
+        }
+        */
+
+        public async Task<IActionResult> Index(string sortOrder, string searchString)
+        {
+            ViewData["property_name"] = String.IsNullOrEmpty(sortOrder) ? "propety_name_desc" : "propety_name";
+            ViewData["address"] = sortOrder == "Address" ? "address=desc" : "Address";
+            ViewData["CurrentFilter"] = searchString;
+            var Properties = from p in context.Properties
+                             select p;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                Properties = Properties.Where(p => p.property_name.Contains(searchString));
+            }
+            switch (sortOrder)
+            {
+                case "propety_name_desc":
+                    Properties = Properties.OrderByDescending(s => s.property_name);
+                    break;
+                case "Address":
+                    Properties = Properties.OrderBy(s => s.address);
+                    break;
+            }
+            return View(await Properties.AsNoTracking().ToListAsync());
+
+            /*
+            public IActionResult ViewProperty(int Property_ID)
+            {
+                    List<Properties> properties = context
+                        .Properties
+                        .Include(property => property.property_name)
+                        .Where(cm => cm.Property_ID == id)
+                        .ToList();
+                    RentPropertyViewModel viewModel = new RentPropertyViewModel
+                    {
+                        Property_name = property_name;
+                        Address = address;
+                    };
+            }
+            */
+
         }
     }
 }
